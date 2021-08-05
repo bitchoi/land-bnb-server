5816e366d57864770d01d1bbf85a32b3db186cfd
bitchoi <choibit753@gmail.com>
2021-08-06 00:28:16 +0900

    initial commit

---
+++ src/main/java/com/bitchoi/landbnbserver/LandBnbServerApplication.java
@@ -0,0 +1,13 @@
+package com.bitchoi.landbnbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandBnbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandBnbServerApplication.class, args);
+    }
+
+}
