57f38a21ac7db9af9c792a55d205a7e7bb6c97fd
bitchoi <choibit753@gmail.com>
2021-08-08 03:15:48 +0900

    Add : passwordEncoder bean

--- src/main/java/com/bitchoi/landbnbserver/LandBnbServerApplication.java
+++ src/main/java/com/bitchoi/landbnbserver/LandBnbServerApplication.java
@@ -2,6 +2,9 @@ package com.bitchoi.landbnbserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LandBnbServerApplication {
@@ -10,4 +13,8 @@ public class LandBnbServerApplication {
         SpringApplication.run(LandBnbServerApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
